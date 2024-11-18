d5a433484f519967381af1e632e49d6498790d8b
karin-zuniga <karin.zuniga.munoz@gmail.com>
2024-11-18 12:15:04 -0300

    archivo basico

---
+++ proyecto_ayed.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+/*Funcion menu?*/
+
+/*abrir archivos y crear arreglos*/
+void abrir_y_leer_archivo(char *arr, string nombre_archivo){
+    ifstream archivo;
+    archivo.open(nombre_archivo);
+    
+}
+
+/*emparejar bases. Matrices*/
+
+//ciclos
+
+
+int main(int argc, char const *argv[])
+{
+    /*inicializar arreglos*/
+    char principal[100] = {0};
+    char secundaria[100] = {0};
+
+
+    /*menu?*/ 
+    return 0;
+}
