58e8b587759ff33da62a537cd3475d0e12d65039
Albani Medina <albanimedina11@gmail.com>
2024-11-20 20:20:02 -0300

    ya se muestra la matriz

--- proyecto_ayed.cpp
+++ proyecto_ayed.cpp
@@ -6,22 +6,21 @@ using namespace std;
 const int MAX_LARGO = 100;
 
 /*abrir archivos y crear arreglos*/
-//le añadi para tener control de errores si es que no se abre el archivo
 int abrir_y_leer_archivo(char *arr, string nombre_archivo){
     ifstream archivo(nombre_archivo); //abre archivo
-    if (!archivo.is_open()) {
+    if (!archivo.is_open()) { //control de errores si es que no se abre el archivo
         cerr << "Error: No se pudo abrir el archivo " << nombre_archivo << endl;
         return 0;
     }
 
     int i = 0;
-     //leer la secuencia completa
-    while (archivo >> arr[i] && i < 100)  { //lee caracter por caracter, maximo tamaño de 1000
+    //lee el archvivo caracter por caracter, maximo tamaño de 1000
+    while (archivo >> arr[i] && i < 100)  { 
         i++;
     }
-    archivo.close();
+    archivo.close(); //se cierra el archivo despues de leer
     arr[i] = '\0'; //termina la cadena
-    return i;
+    return i; //long de la secuencia leida
 }
     
 //     int i = 0;
@@ -50,17 +49,17 @@ void compara_bases(char *prin, char *sec, string primer_archivo, string segundo_
         cerr << "Error: No se pudieron leer las secuencias." << endl;
         return;
     }
-        // int mayor;
-    // if (m > n)
-    // {
-    //     mayor = m;
-    // }
-    // else
-    // {
-    //     mayor = n;
-    // }
+        int mayor;
+    if (m > n)
+    {
+        mayor = m;
+    }
+    else
+    {
+        mayor = n;
+    }
 
-    int mayor = max(m, n); //determina la longitud maxima
+    //int mayor = max(m, n); //determina la longitud maxima
     int matriz[100][100] = {0}; //se crea la matriz estarica
 
     //parametros del algoritmo
@@ -83,7 +82,7 @@ void compara_bases(char *prin, char *sec, string primer_archivo, string segundo_
             int arriba = matriz[i-1][j] + gap;
             int izq = matriz[i][j-1] + gap;
             int diagonal;
-            //ahora compara las bases para ver si coincide o no
+            //compara las bases para ver si coincide o no
             if (prin[i-1] == sec[j-1]) {
                 diagonal = matriz [i-1][j-1] + match;
             } else {
@@ -96,7 +95,8 @@ void compara_bases(char *prin, char *sec, string primer_archivo, string segundo_
             // {
             //     int diagonal = matriz[i-1][j-1] + mismatch;
             // }
-            matriz[i][j]= max({diagonal, arriba, izq});
+            matriz[i][j]= max(diagonal, max(arriba, izq));
+            //maximo valor de las opciones posibles
             
         }
         
@@ -105,23 +105,76 @@ void compara_bases(char *prin, char *sec, string primer_archivo, string segundo_
     cout << "Matriz de puntuaciones: " << endl;
     for (int i = 0; i <=m; i++) {
         for (int j = 0; j <= n; j++) {
-            cout << matriz [i][j]<< "/t";
+            cout << matriz [i][j]<< "\t";
         }
         cout << endl;
     }
 
     //ahora la reconstruccion del alineamiento desde la matriz
-    string alineamiento1 = ""; //arreglo para la secuencia1 alineada
-    string alineamiento2 = ""; //arreglo para la secuencia2 alineada
+    string alineamiento1 = ""; //secuencia1 alineada
+    string alineamiento2 = ""; //secuencia2 alineada
     int posicion = 199; //variable de seguimiento de posicion actual 
     //199 corresponde a la ultima posicion del arreglo y ahi comienza
 
     int i = m;
     int j = n;
 
+    //para generar la imagen en graphiz
+    ofstream archivo_dot("alineamiento.dot");
+    archivo_dot << "digraph Alineamiento {" << endl;
+    archivo_dot << "rankdir=LR;" << endl;
+    int nodo_id = 0;
 
+    //se compara la matriz para el alineamiento
+    while (i > 0 || j > 0) {
 
+        if (matriz[i][j] == matriz[i-1][j-1] + match) { //coincidencia
+            if (prin[i-1] == sec[j-1]) {
+                alineamiento1 = prin[i-1] + alineamiento1;
+                alineamiento2 = sec[j-1] + alineamiento2;
+                i--;
+                j--;
+            }
 
+        } else if (matriz[i][j] == matriz [i-1][j-1] + mismatch) { //no coinciden
+            if (prin[i-1] != sec[j-1]) {
+                alineamiento1 = prin[i-1] + alineamiento1;
+                alineamiento2 = sec[j-1] + alineamiento2;
+                i--;
+                j--;
+            }
+
+        } else if (matriz[i][j] == matriz[i-1][j] + gap) { //gap en la segunda secuencia
+            alineamiento1 = prin[i-1] + alineamiento1;
+            alineamiento2 = '-' + alineamiento2;
+            i--;
+
+        } else { //gap en la primera secuencia
+            alineamiento1 = '-' + alineamiento1;
+            alineamiento2 = sec[j-1] + alineamiento2;
+            j--;
+        }
+    }
+
+    while (i > 0) {
+        alineamiento1 = prin[i-1] + alineamiento1;
+        alineamiento2 = '-' + alineamiento2;
+        i--;
+    }
+
+    while (j > 0) {
+        alineamiento1 = '-' + alineamiento1;
+        alineamiento2 = sec[j - 1] + alineamiento2;
+        j--;
+    }
+
+    cout << "Alineamiento1: " << alineamiento1 << endl;
+    cout << "ALineamiento2: " << alineamiento2 << endl;
+
+    //deberia generar la imagen
+    //no son las mismas lineas de codigo para windows o linux :)
+    system("dot -Tpng alineamiento.dot -o alineamiento.png");
+    system("start alineamiento.png");
 }
 
 
@@ -130,11 +183,11 @@ int main(int argc, char const *argv[]) {
     const int largo = 100;
     char principal[largo] = {0};
     char secundaria[largo] = {0};
-    int gap = -2; //en -2 para que no sea igual que el -1 por el mismatch
+    int gap = -1; //en -2 para que no sea igual que el -1 por el mismatch de no coincidir
 
     //lee secuencias desde archivos
-    string archivo_principal = "principal.txt";
-    string archivo_secundaria = "secundaria.txt";
+    string archivo_principal = "secuencia1.txt";
+    string archivo_secundaria = "secuencia2.txt";
     int m = abrir_y_leer_archivo(principal, archivo_principal);
     int n = abrir_y_leer_archivo(secundaria, archivo_secundaria);
 
@@ -143,8 +196,6 @@ int main(int argc, char const *argv[]) {
         return 0;
     }
     compara_bases(principal, secundaria, archivo_principal, archivo_secundaria, gap);
-
-
     // int i = 0;
     // string nombre = "prueba.txt";
     // int largo_real = abrir_y_leer_archivo(principal, nombre);
@@ -155,39 +206,5 @@ int main(int argc, char const *argv[]) {
     // }
     // cout << endl;
 
- 
-    return 0;
+    
 }
-
-
-//     se inicializa la primera fila y columna (rellenadas con los puntajes por gaps)
-// se llena la matriz de puntuacion segun las tres opciones posibles:
-//     1.emparejar los caracteres
-//     2.insertar un gap en la secuencia principal
-//     3.insertar un gap en la secuencia secundaria
-//     for (int i = 1; i <= m; i++) {
-//         for (int j = 1; j <= n; j++) {
-//             calcular la puntuacion para la opcion de emparejar los caracteres
-//             int diagonal;
-//             if (prin[i - 1] == sec[j - 1]) {
-//                 diagonal = matriz[i - 1][j - 1] + match; //coinciden, se suman puntos por match
-//             } else {
-//                 diagonal = matriz[i - 1][j - 1] + mismatch; //como no coinciden, se suman puntos por mismatch
-//             }
-            
-//             se calcula la puntuacion por gap en la secuencia principal
-//             int arriba = matriz[i - 1][j] + gap;
-            
-//             calcula la puntuacion por gap en la secuencia secundaria
-//             int izquierda = matriz[i][j - 1] + gap;
-            
-//             slmacenar la maxima puntuacion de las tres opciones
-//             matriz[i][j] = max(diagonal, max(arriba, izquierda));
-//         }
-//     }
-
-//     reconstruccion del alineamiento desde la matriz de puntuacion
-//     string alineamiento1 = "", alineamiento2 = "";
-//     int i = m, j = n;
-
-//     ahora hay que recorrerla para encontrar el alineamiento correcto :)
